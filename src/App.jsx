c03059d032dcc8f4fbe014f31b98c090629d1bd2
Niranjan <niranjanpurushothaman@Niranjans-MacBook-Air.local>
2026-02-20 16:38:02 +0530

    My First Project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Invoice from './components/Invoice'
+
+function App() {
+  return (
+    <div className='bg-slate-200 min-h-screen p-2'>
+      <Invoice />
+    </div>
+  )
+}
+
+export default App
